1c444a87efdff5ff6067d3e0899f312e3f988d3a
Felix Söderström <felix.f.soderstrom@gmail.com>
2024-10-03 08:48:47 +0200

    Added more logging
    After a day of waiting, Azure finally updated the code from this repo.
    It might have something to do with the fact that im using free tier.
    The refresh button on the Azure portal is un-clickable so..
    I guess we're taking the scenic route eh?

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from chat import ChatHandler
 import os
 import logging
 
+# Please Bill..
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -20,20 +22,32 @@ BOT = ChatHandler()
 
 async def messages(req: web.Request) -> web.Response:
     logger.info("RECIEVED A MESSAGE")
+
     if "application/json" in req.headers["Content-Type"]:
         body = await req.json()
+        logger.info(f"RECIEVED BODY: {body}")
     else:
+        logger.warning("RECIEVED NON-JSON CONTENT.")
         return web.Response(status=415)
     
     activity = Activity().deserialize(body)
     auth_header = req.headers["Authorization"] if "Authorization" in req.headers else""
+    logger.info("PROCESSING ACTIVITY")
+
     response = await ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
+    logger.info("ACTIVITY PROCESSED")
+
     if response:
+        logger.info(f"SENDING RESPONSE: {response.body}")
         return web.json_response(data=response.body, status=response.status)
+    
+    logger.info("NO RESPONSE TO SEND")
     return web.Response(status=201)
+
 
 APP = web.Application()
 APP.router.add_post("/api/messages", messages)
+
 
 if __name__ == "__main__":
     try:
